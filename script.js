acdb493c67159a326ca8ceb05d2fd10bf41e8a7c
Dowody <dowody2007@gmail.com>
2024-12-24 22:20:52 +0200

    1.2

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector(".top-nav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add("hidden");
+    } else {
+        nav.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+});
