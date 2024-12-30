a75d61ac2b4ebf6c5d403c112e898ed4799ce586
Dowody <dowody2007@gmail.com>
2024-12-31 00:13:46 +0200

    3.0

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+// window.addEventListener('load', () => {
+//     const preloader = document.getElementById('preloader');
+//     preloader.classList.add('hidden');
+//     setTimeout(() => {
+//         preloader.style.display = 'none';
+//     }, 500);
+// });
 window.addEventListener('load', () => {
     const preloader = document.getElementById('preloader');
     preloader.classList.add('hidden');
     setTimeout(() => {
         preloader.style.display = 'none';
+        let preloadImages = document.querySelectorAll('.preload-excluded');
+        preloadImages.forEach(function(image) {
+            image.style.visibility = 'visible';
+        });
     }, 500);
 });
+
 
 const nav = document.querySelector(".top-nav");
 let lastScrollY = window.scrollY; 
